d1c822ee0412b6512e4f8e34f8503f296bca4332
Taras Havrysh <ter4444@gmail.com>
2015-06-19 16:29:24 +0700

    accomplished contact page

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,8 @@ $(window).bind("load", function () {
     var footer = $("#footer");
     var pos = footer.position();
     var height = $(window).height();
+
+    $('#footer').css('visibility', 'visible');
     height = height - pos.top;
     height = height - footer.height();
     if (height > 0) {
