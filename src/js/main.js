59c1c94adf29f9a792b3abfdd0923b08384b895b
Taras Havrysh <ter4444@gmail.com>
2015-06-19 16:11:05 +0700

    accomplished about page

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,16 @@ var $ = require('jquery');
 var foundation = require('foundation');
 
 $(document).foundation();
+
+$(window).bind("load", function () {
+    var footer = $("#footer");
+    var pos = footer.position();
+    var height = $(window).height();
+    height = height - pos.top;
+    height = height - footer.height();
+    if (height > 0) {
+        footer.css({
+            'margin-top': height + 'px'
+        });
+    }
+});
